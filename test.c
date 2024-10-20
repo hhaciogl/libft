ac9f8af3cf75f4b02bad71e68fb5a689875f6e09
hhaciogl <hhaciogl@student.42istanbul.com.tr>
2024-10-20 17:13:00 +0300

    update

--- test.c
+++ test.c
@@ -18,7 +18,6 @@ static void test_case_ft_isalpha_empty(void)
 {
 	test(ft_isalpha(), "ft_isalpha() should return 1");
 }
-
 static void test_case_ft_isalpha_4(void)
 {
 	int result = ft_isalpha();
@@ -27,7 +26,6 @@ static void test_case_ft_isalpha_4(void)
 	if(DEBUG)
 		printf(" result: %i\n expected:%i\n", result, expected);
 }
-
 static void test_suite_ft_isalpha(void)
 {
 	printf("%s:\n", __func__);
@@ -35,10 +33,28 @@ static void test_suite_ft_isalpha(void)
 	test_case_ft_isalpha_4();
 }
 
+// examples
+// static void test_case_ft_isalpha_4(void)
+// {
+// 	int result = ft_isalpha();
+// 	int expected = 0;
+// 	test(ft_isalpha()== 1, "ft_isalpha(4) should return 0");
+// 	if(DEBUG)
+// 		printf(" result: %i\n expected:%i\n", result, expected);
+// }
+
+// static void test_suite_ft_isalpha(void)
+// {
+// 	printf("%s:\n", __func__);
+// 	test_case_ft_isalpha_empty();
+// 	test_case_ft_isalpha_4();
+// }
+
 int main(void)
 {
 	test_suite_ft_isalpha();
+	
 
-	puts("\n\n\n\n---TESTING FINISED---");
+	puts("\n\n\n---TESTING FINISED---");
 	return 0;//
 }
