10f8091ee1e1c7b817524dc4c9cf80dee7e9b81b
Hasan Hacioglu <hhaciogl@k1m10s02.42istanbul.com.tr>
2024-10-23 20:13:09 +0300

    update

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hhaciogl <hhaciogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 19:24:13 by hhaciogl          #+#    #+#             */
-/*   Updated: 2024/10/23 19:44:08 by hhaciogl         ###   ########.fr       */
+/*   Updated: 2024/10/23 20:11:51 by hhaciogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && n > 0)
+	while ((s1[i] || s2[i]) && n > 0)
 	{
 		
-		if (s1[i] != s2[i])
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
 		{
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		}
 		n--;
 		i++;
