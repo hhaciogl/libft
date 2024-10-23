7eed7c1c73ee3516e9580c7d3a0d9530e57b9761
Hasan Hacioglu <hhaciogl@k1m10s02.42istanbul.com.tr>
2024-10-23 15:55:04 +0300

    update

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,22 +6,15 @@
 /*   By: hhaciogl <hhaciogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 13:50:27 by hhaciogl          #+#    #+#             */
-/*   Updated: 2024/10/23 14:35:27 by hhaciogl         ###   ########.fr       */
+/*   Updated: 2024/10/23 15:53:42 by hhaciogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int	ft_is_lower(int c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	return (0);
-}
-
 int	ft_tolower(int c)
 {
-	if (!ft_is_lower(c))
+	if (c >= 'A' && c <= 'Z')
 		return (c + ('a' - 'A'));
 	return (c);
 }
--- ft_toupper.c
+++ ft_toupper.c
@@ -6,22 +6,15 @@
 /*   By: hhaciogl <hhaciogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 13:48:21 by hhaciogl          #+#    #+#             */
-/*   Updated: 2024/10/23 14:17:04 by hhaciogl         ###   ########.fr       */
+/*   Updated: 2024/10/23 15:52:41 by hhaciogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int	ft_is_upper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	return (0);
-}
-
 int	ft_toupper(int c)
 {
-	if (!ft_is_upper(c))
+	if (c >= 'a' && c <= 'z')
 		return (c - ('a' - 'A'));
 	return (c);
 }
