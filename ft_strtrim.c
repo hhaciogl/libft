76cca23f6277f62b144472187158d05f7512b3f0
Hasan Hacioglu <hhaciogl@k1m12s01.42istanbul.com.tr>
2024-10-28 17:48:02 +0300

    update

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hhaciogl <hhaciogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 22:38:35 by hhaciogl          #+#    #+#             */
-/*   Updated: 2024/10/27 23:22:05 by hhaciogl         ###   ########.fr       */
+/*   Updated: 2024/10/28 17:47:57 by hhaciogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	len;
 	size_t	start;
 	size_t	end;
 	int		is_start_found;
 	int		is_end_found;
 
-	len = ft_strlen(s1);
-	end = len - 1;
+	end = ft_strlen(s1);
 	start = 0;
 	is_end_found = 0;
 	is_start_found = 0;
@@ -31,10 +29,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 			start++;
 		else
 			is_start_found = 1;
-		if (!is_end_found && ft_strchr(set, s1[end]))
+		if (!is_end_found && ft_strchr(set, s1[end - 1]))
 			end--;
 		else
 			is_end_found = 1;
 	}
-	return (ft_substr(s1, start, end + 1 - start));
+	return (ft_substr(s1, start, end - start));
 }
