416a9127dee17b534204d2590c15694a96a09ee0
Hasan Hacioglu <hhaciogl@k1m12s01.42istanbul.com.tr>
2024-10-28 12:46:59 +0300

    update

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hhaciogl <hhaciogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 16:34:44 by hhaciogl          #+#    #+#             */
-/*   Updated: 2024/10/27 16:44:24 by hhaciogl         ###   ########.fr       */
+/*   Updated: 2024/10/28 12:45:29 by hhaciogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@ void	*ft_calloc(size_t num_of_elements, size_t size_of_element)
 
 	size_of_memory = num_of_elements * size_of_element;
 	piece_of_memory = malloc(size_of_memory);
+	if (piece_of_memory == NULL)
+		return (NULL);
 	return (ft_memset(piece_of_memory, 0, size_of_memory));
 }
