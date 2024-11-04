9a65ea44489d2630b04271dd59e6b9f1b2ba6254
Hasan Hacioglu <hhaciogl@k1m07s11.42istanbul.com.tr>
2024-11-04 14:02:36 +0300

    update

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,18 +6,18 @@
 /*   By: hhaciogl <hhaciogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 16:24:34 by hhaciogl          #+#    #+#             */
-/*   Updated: 2024/11/03 19:56:07 by hhaciogl         ###   ########.fr       */
+/*   Updated: 2024/11/04 13:58:52 by hhaciogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	char	to_char(int n)
+static	char	to_char(long n)
 {
 	return ("0123456789"[n]);
 }
 
-static	void	write_char(int n, int fd)
+static	void	write_char(long n, int fd)
 {
 	char	c;
 
@@ -29,14 +29,14 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	long	ln;
 
-	ln = n;
+	ln = (long)n;
 	if (n < 0)
 	{
 		ln = ln * -1;
 		ft_putchar_fd('-', fd);
 	}
 	if (ln < 10 && ln >= 0)
-		write_char(n, fd);
+		write_char(ln, fd);
 	else
 	{
 		ft_putnbr_fd(ln / 10, fd);
